33f4f7c9d9e11da358e4e39cd410424eb8ef5717
Rushane Barnes <barnesrushane@hotmail.com>
2019-03-31 16:50:52 -0500

    Home page interface

---
+++ app/src/main/java/com/example/android/colourtales/RedRidingHoodActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.colourtales;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RedRidingHoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_red_riding_hood);
+    }
+}
