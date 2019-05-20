0aa33d550d3361e97726a17aa52af6bb3d508a9d
Rushane Barnes <barnesrushane@hotmail.com>
2019-05-19 23:38:49 -0500

    Added TextView and TextToSpeech

--- app/src/main/java/com/example/android/colourtales/RedRidingHoodActivity.java
+++ app/src/main/java/com/example/android/colourtales/RedRidingHoodActivity.java
@@ -1,13 +1,281 @@
 package com.example.android.colourtales;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.view.View.OnTouchListener;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.speech.tts.TextToSpeech;
 
-public class RedRidingHoodActivity extends AppCompatActivity {
+import com.devs.vectorchildfinder.VectorChildFinder;
+import com.devs.vectorchildfinder.VectorDrawableCompat;
 
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.Queue;
+
+public class RedRidingHoodActivity extends AppCompatActivity implements OnTouchListener, TextToSpeech.OnInitListener {
+
+    //private RelativeLayout drawingLayout;
+    private RelativeLayout drawingLayout;
+    private MyView myView;
+    Button red, blue, yellow;
+    Paint paint;
+    TextToSpeech textToSpeech;
+    String speakText;
+    /** Called when the activity is first created. */
+    /*
+     *
+     * private ImageView imageView; private Canvas cv; private Bitmap mask,
+     * original, colored; private int r,g,b; private int sG, sR, sB;
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_red_riding_hood);
+
+        speakText = getString(R.string.six_to_eight_red_one_full);
+        textToSpeech = new TextToSpeech(this, this);
+        texttoSpeak();
+        myView = new MyView(this);
+        drawingLayout = (RelativeLayout) findViewById(R.id.relative_layout);
+        drawingLayout.addView(myView);
+
+        red = (Button) findViewById(R.id.btn_red);
+        blue = (Button) findViewById(R.id.btn_blue);
+        yellow = (Button) findViewById(R.id.btn_yellow);
+
+        red.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                paint.setColor(Color.RED);
+            }
+        });
+
+        yellow.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                paint.setColor(Color.YELLOW);
+            }
+        });
+        blue.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                paint.setColor(Color.BLUE);
+            }
+        });
     }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = textToSpeech.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("error", "This Language is not supported");
+            } else {
+                texttoSpeak();
+            }
+        } else {
+            Log.e("error", "Failed to Initialize");
+        }
+    }
+    @Override
+    public void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
+    }
+    private void texttoSpeak() {
+        String text = speakText;
+        if ("".equals(text)) {
+            text = "Please enter some text to speak.";
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
+        else {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    public class MyView extends View {
+
+        private Path path;
+        Bitmap mBitmap;
+        ProgressDialog pd;
+        final Point p1 = new Point();
+        Canvas canvas;
+
+        // Bitmap mutableBitmap ;
+        public MyView(Context context) {
+            super(context);
+
+            paint = new Paint();
+            paint.setAntiAlias(true);
+            pd = new ProgressDialog(context);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeJoin(Paint.Join.ROUND);
+            paint.setStrokeWidth(5f);
+            mBitmap = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.six_to_eight_first_pic).copy(Bitmap.Config.ARGB_8888, true);
+
+            this.path = new Path();
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            this.canvas = canvas;
+            paint.setColor(Color.GREEN);
+            canvas.drawBitmap(mBitmap, 0, 0, paint);
+
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            float x = event.getX();
+            float y = event.getY();
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+
+                    p1.x = (int) x;
+                    p1.y = (int) y;
+                    final int sourceColor = mBitmap.getPixel((int) x, (int) y);
+                    final int targetColor = paint.getColor();
+                    new TheTask(mBitmap, p1, sourceColor, targetColor).execute();
+                    invalidate();
+            }
+            return true;
+        }
+
+        public void clear() {
+            path.reset();
+            invalidate();
+        }
+
+        public int getCurrentPaintColor() {
+            return paint.getColor();
+        }
+
+        class TheTask extends AsyncTask<Void, Integer, Void> {
+
+            Bitmap bmp;
+            Point pt;
+            int replacementColor, targetColor;
+
+            public TheTask(Bitmap bm, Point p, int sc, int tc) {
+                this.bmp = bm;
+                this.pt = p;
+                this.replacementColor = tc;
+                this.targetColor = sc;
+                pd.setMessage("Filling....");
+                pd.show();
+            }
+
+            @Override
+            protected void onPreExecute() {
+                pd.show();
+
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+
+            }
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                FloodFill f = new FloodFill();
+                f.floodFill(bmp, pt, targetColor, replacementColor);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                pd.dismiss();
+                invalidate();
+            }
+        }
+    }
+
+// flood fill
+
+    public class FloodFill {
+        public void floodFill(Bitmap image, Point node, int targetColor,
+                              int replacementColor) {
+            int width = image.getWidth();
+            int height = image.getHeight();
+            int target = targetColor;
+            int replacement = replacementColor;
+            if (target != replacement) {
+                Queue<Point> queue = new LinkedList<Point>();
+                do {
+
+                    int x = node.x;
+                    int y = node.y;
+                    while (x > 0 && image.getPixel(x - 1, y) == target) {
+                        x--;
+
+                    }
+                    boolean spanUp = false;
+                    boolean spanDown = false;
+                    while (x < width && image.getPixel(x, y) == target) {
+                        image.setPixel(x, y, replacement);
+                        if (!spanUp && y > 0
+                                && image.getPixel(x, y - 1) == target) {
+                            queue.add(new Point(x, y - 1));
+                            spanUp = true;
+                        } else if (spanUp && y > 0
+                                && image.getPixel(x, y - 1) != target) {
+                            spanUp = false;
+                        }
+                        if (!spanDown && y < height - 1
+                                && image.getPixel(x, y + 1) == target) {
+                            queue.add(new Point(x, y + 1));
+                            spanDown = true;
+                        } else if (spanDown && y < height - 1
+                                && image.getPixel(x, y + 1) != target) {
+                            spanDown = false;
+                        }
+                        x++;
+                    }
+                } while ((node = queue.poll()) != null);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
 }
