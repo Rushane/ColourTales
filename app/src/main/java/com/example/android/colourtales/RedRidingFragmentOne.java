00dc443ad5b83f2703fad7dc55de28f16394c517
Rushane Barnes <barnesrushane@hotmail.com>
2019-05-21 12:47:54 -0500

    Added additional activities and layouts

--- app/src/main/java/com/example/android/colourtales/RedRidingFragmentOne.java
+++ app/src/main/java/com/example/android/colourtales/RedRidingFragmentOne.java
@@ -2,7 +2,6 @@ package com.example.android.colourtales;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -10,109 +9,87 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.view.View.OnTouchListener;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.speech.tts.TextToSpeech;
-
-import com.devs.vectorchildfinder.VectorChildFinder;
-import com.devs.vectorchildfinder.VectorDrawableCompat;
 
 import java.util.LinkedList;
 import java.util.Locale;
 import java.util.Queue;
 
-public class RedRidingHoodActivity extends AppCompatActivity implements OnTouchListener, TextToSpeech.OnInitListener {
 
-    //private RelativeLayout drawingLayout;
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link RedRidingFragmentOne.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link RedRidingFragmentOne#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RedRidingFragmentOne extends Fragment implements OnTouchListener, TextToSpeech.OnInitListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
     private RelativeLayout drawingLayout;
     private MyView myView;
     Button red, blue, yellow;
-    ImageButton previousButton, nextButton;
     Paint paint;
     TextToSpeech textToSpeech;
     String speakText;
+    Context context;
 
-    /** Called when the activity is first created. */
-    /*
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public RedRidingFragmentOne() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
      *
-     * private ImageView imageView; private Canvas cv; private Bitmap mask,
-     * original, colored; private int r,g,b; private int sG, sR, sB;
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RedRidingFragmentOne.
      */
+    // TODO: Rename and change types and number of parameters
+    public static RedRidingFragmentOne newInstance(String param1, String param2) {
+        RedRidingFragmentOne fragment = new RedRidingFragmentOne();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_red_riding_hood);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
 
-        speakText = getString(R.string.six_to_eight_red_one_full);
-        textToSpeech = new TextToSpeech(this, this);
-        texttoSpeak();
-        myView = new MyView(this);
-        drawingLayout = (RelativeLayout) findViewById(R.id.relative_layout);
-        drawingLayout.addView(myView);
-
-        previousButton = (ImageButton)findViewById(R.id.previousarrow);
-        nextButton = (ImageButton)findViewById(R.id.nextarrow);
-
-        red = (Button) findViewById(R.id.btn_red);
-        blue = (Button) findViewById(R.id.btn_blue);
-        yellow = (Button) findViewById(R.id.btn_yellow);
-
-
-        red.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                paint.setColor(Color.RED);
-            }
-        });
-
-        yellow.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                paint.setColor(Color.YELLOW);
-            }
-        });
-        blue.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                paint.setColor(Color.BLUE);
-            }
-        });
-
-        previousButton.setOnClickListener(new OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                finish();
-            }
-        });
-
-
-        nextButton.setOnClickListener(new OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent loginIntent = new Intent(RedRidingHoodActivity.this, RedRidingHoodActivityTwo.class);
-                startActivity(loginIntent);
-            }
-        });
 
     }
 
@@ -150,9 +127,90 @@ public class RedRidingHoodActivity extends AppCompatActivity implements OnTouchL
         }
     }
 
-    @Override
+    /*@Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    } */
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.riding_fragment_first, container, false);
+        speakText = getString(R.string.six_to_eight_red_two_full);
+        //textToSpeech = new TextToSpeech(this,this );
+        texttoSpeak();
+
+        //myView = new MyView(view);
+        drawingLayout = (RelativeLayout)view.findViewById(R.id.relative_layout);
+        drawingLayout.addView(myView);
+
+        red = (Button)view.findViewById(R.id.btn_red);
+        blue = (Button)view.findViewById(R.id.btn_blue);
+        yellow = (Button)view.findViewById(R.id.btn_yellow);
+
+        red.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                paint.setColor(Color.RED);
+            }
+        });
+
+        yellow.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                paint.setColor(Color.YELLOW);
+            }
+        });
+        blue.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                paint.setColor(Color.BLUE);
+            }
+        });
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 
     public class MyView extends View {
@@ -305,5 +363,4 @@ public class RedRidingHoodActivity extends AppCompatActivity implements OnTouchL
         // TODO Auto-generated method stub
         return false;
     }
-
 }
